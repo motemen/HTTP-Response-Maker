967ac0cc4ee29452dd3d1a0013ed94a3cfe5fcfb
motemen <motemen@gmail.com>
2012-03-21 10:13:12 +0900

    initial commit

---
+++ lib/HTTP/Response/Maker.pm
@@ -0,0 +1,34 @@
+package HTTP::Response::Maker;
+use strict;
+use warnings;
+
+our $VERSION = '0.01';
+
+1;
+
+__END__
+
+=head1 NAME
+
+HTTP::Response::Maker -
+
+=head1 SYNOPSIS
+
+  use HTTP::Response::Maker;
+
+=head1 DESCRIPTION
+
+HTTP::Response::Maker is
+
+=head1 AUTHOR
+
+motemen E<lt>motemen@gmail.comE<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
